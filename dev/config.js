7018695c395b2d840611cb7153771159a209ebe7
Abhishek Saha <abhisheksaha11@gmail.com>
2017-08-29 01:10:39 +0200

    bugfix

--- dev/config.js
+++ dev/config.js
@@ -7,14 +7,4 @@ module.exports = new function() {
     this.getEndpoint = function() {
         return window.mode == "dev" ? this.dev : this.prod;
     };
-
-    this.appId = "amaekfehpldjnkblldhakfiilflijkcl";
-
-    this.clientId =
-        "145450808466-nvvknusqqjqo70s9b68d2k9luen6tr1d.apps.googleusercontent.com";
-
-    this.clientSecret = "S_F1bEMnwRGdWR4dEgUwmVBa";
-
-    this.refreshToken =
-        "1/KtWQNQgDZlQufuuG4_SSjxSVbZ9s_0Y730q1nu8FWfq0mrpU3luN-8cgtvPB6qIq";
 }();
